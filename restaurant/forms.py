6a393a346b4807bd6a1212e2725114920f187c08
Aubrey Agub <2555752a@student.gla.ac.uk>
2023-03-09 18:02:49 +0000

    forms and models for Restaurant, Review, Menu, MenuItem

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from restaurant.models import Page, Category
+from restaurant.models import Restaurant, Review, Menu, MenuItem
 from django.contrib.auth.models import User
 from restaurant.models import UserProfile
 
@@ -42,3 +43,57 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('website', 'picture',)
+
+class RestaurantForm(forms.ModelForm):
+    restaurantID = forms.CharField(max_length=Restaurant.RESTAURANT_ID_MAX_LENGTH, help_text="Restaurant name:")
+    location = forms.CharField(max_length=Restaurant.RESTAURANT_LOCATION_MAX_LENGTH, help_text="Location:")
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    class Meta:
+        model = Restaurant
+        fields = ('restaurantID', 'location', 'photo',)
+
+class MenuForm(forms.ModelForm):
+    menuID = forms.CharField(max_length=Menu.MENU_ID_MAX_LENGTH, help_text="Menu name:")
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    class Meta:
+        model = Menu
+        fields = ('menuID',)
+    def clean(self):
+         cleaned_data = self.cleaned_data
+         url = cleaned_data.get('url')
+         if url and not url.startswith('http://'):
+             url = f'http://{url}'
+             cleaned_data['url'] = url
+         return cleaned_data
+    
+class MenuItemForm(forms.ModelForm):
+    menuItemID = forms.CharField(max_length=MenuItem.MENUITEM_ID_MAX_LENGTH, help_text="Menu item name:")
+    description = forms.CharField(max_length=MenuItem.DESCRIPTION_MAX_LENGTH, help_text="Describe this menu item:")
+    price = forms.FloatField(initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    class Meta:
+        model = MenuItem
+        fields = ('menuItemID', 'description', 'price', 'photo',)
+    def clean(self):
+         cleaned_data = self.cleaned_data
+         url = cleaned_data.get('url')
+         if url and not url.startswith('http://'):
+             url = f'http://{url}'
+             cleaned_data['url'] = url
+         return cleaned_data
+    
+class ReviewForm(forms.ModelForm):
+    title = forms.CharField(max_length=Review.REVIEW_ID_MAX_LENGTH, help_text="Comment title:")
+    comment = forms.CharField(max_length=Review.COMMENT_MAX_LENGTH, help_text="What did you think about this menu item?")
+    rating = forms.IntegerField(initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    class Meta:
+        model = Review 
+        fields = ('title', 'comment', 'rating', 'photo',)
+    def clean(self):
+         cleaned_data = self.cleaned_data
+         url = cleaned_data.get('url')
+         if url and not url.startswith('http://'):
+             url = f'http://{url}'
+             cleaned_data['url'] = url
+         return cleaned_data
--- restaurant/models.py
+++ restaurant/models.py
@@ -38,3 +38,57 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+class Restaurant(models.Model):
+    RESTAURANT_ID_MAX_LENGTH=128
+    RESTAURANT_LOCATION_MAX_LENGTH=200
+ 
+    restaurantID = models.CharField(max_length=RESTAURANT_ID_MAX_LENGTH, unique=True) # primary key
+    location = models.CharField(max_length=RESTAURANT_LOCATION_MAX_LENGTH, default="None")
+    photo = models.ImageField(upload_to='restaurant_images', blank=True)
+    slug = models.SlugField(unique=True)
+    def save(self, *args, **kwards):
+        self.slug = slugify(self.restaurantID)
+        super(Restaurant, self).save(*args, **kwards)
+    def __str__(self):
+        return self.restaurantID
+    
+class Menu(models.Model):
+    MENU_ID_MAX_LENGTH=128
+    menuID = models.TextField(max_length=MENU_ID_MAX_LENGTH,unique=True) # primary key
+    restaurant = models.OneToOneField(Restaurant, on_delete=models.CASCADE) # one to one relationship - one menu for one restaurant
+    slug = models.SlugField(unique=True)
+    def save(self, *args, **kwards):
+        self.slug = slugify(self.menuID)
+        super(Menu, self).save(*args, **kwards)
+    def __str__(self):
+        return self.menuID
+    
+class MenuItem(models.Model):
+    MENUITEM_ID_MAX_LENGTH=128
+    DESCRIPTION_MAX_LENGTH=200
+    menuItemID = models.CharField(max_length=MENUITEM_ID_MAX_LENGTH, unique=True) # primary key 
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE) # foreign key: one to many - many menu items for one menu
+    description = models.CharField(max_length=DESCRIPTION_MAX_LENGTH, default="None")
+    price = models.FloatField(default =0.0)
+    photo = models.ImageField(upload_to='menu_item_images', blank=True)
+    slug = models.SlugField(unique=True)
+    def save(self, *args, **kwards):
+        self.slug = slugify(self.menuItemID)
+        super(MenuItem, self).save(*args, **kwards)
+    class Meta:
+        verbose_name_plural = 'MenuItems'
+    def __str__(self):
+        return self.menuItemID
+    
+class Review(models.Model):
+    REVIEW_ID_MAX_LENGTH=128
+    COMMENT_MAX_LENGTH=1000
+    reviewID =  models.CharField(max_length=REVIEW_ID_MAX_LENGTH, unique=True) # primary key 
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # foreign key: one to many - many reviews for one user 
+    menuItem = models.ForeignKey(MenuItem, on_delete=models.CASCADE) # foreign key: one to many - many reviews for one menu item
+    comment = models.CharField(max_length=COMMENT_MAX_LENGTH)
+    ratings = models.IntegerField(default =0)
+    photo = models.ImageField(upload_to='review_images', blank=True)
+    def __str__(self):
+        return self.reviewID
+    

7e02fe4f8709040996204117a7ef0ae8b497e068
Aubrey Agub <2555752a@student.gla.ac.uk>
2023-03-18 20:46:29 +0000

    finished population script inclusing reviews - all working

--- populate_restaurant.py
+++ populate_restaurant.py
@@ -7,20 +7,75 @@ django.setup()
 from restaurant.models import Menu,MenuItem,Review
 
 def populate():
+    # Spaghetti Reviews
+    spaghetti_reviews = [
+        {'reviewID': 'Delicious spaghetti',
+         'user_id':2,
+         'comment': 'Lovely sauce, not too sweet. Made with fresh ingredients and good quality pasta. ',
+         'ratings': 5,
+         'photo': None},
+        {'reviewID': 'A tad expensive',
+         'user_id':2,
+         'comment': 'Good pasta however a bit more expensive than standard. The portion was also too small for me.',
+         'ratings': 3,
+         'photo': None}
+    ]
+    # Chicken Burger Reviews
+    chickenBurger_reviews = [
+        {'reviewID': 'HORRIBLE, never again.',
+         'user_id':2,
+         'comment': 'I usually enjoy most things I get from this place, however their chicken burger is absolutely horrendous. Mine came undercooked and still very pink inside. Burger was dry and I think I saw mould, gross! NEVER ORDER THIS!!!',
+         'ratings': 0,
+         'photo': None},
+        {'reviewID': 'Decent but not amazing',
+         'user_id':2,
+         'comment': "I've had better burgers, it won't hurt to eat but wouldn't have it again.",
+         'ratings': 3,
+         'photo': None},
+         {'reviewID': 'Uncooked and mouldy!',
+          'user_id':2,
+         'comment': "Gross gross gross! My burger if I can even call it that came frozen (how?> even I don't know) and the bread had green stuff on it! Never get this.",
+         'ratings': 1,
+         'photo': None}
+    ]
+    # Sweet Potato Fries Reviews
+    spFries_reviews = [{'reviewID': 'Unique side',
+         'user_id':2,
+         'comment': "They have an interesting texture, definitely not as crispy as normal potato fries but tastes very very good. I'd reccomend to try if you're up for something different.",
+         'ratings': 4,
+         'photo': None}
+         ]
+    hotChocolate_reviews = [
+        {'reviewID': 'Yummy drink',
+         'user_id':2,
+         'comment': "Hands down one of if not THE BEST hot chocolate in the city! Bonus points for being vegan too. And the marshmallow were soooooooo good. Definitely order this if you're ever here!",
+         'ratings': 5,
+         'photo': None}]
+    # Orange Juice Reviews
+    oj_reviews = [
+        {'reviewID': 'Fresh juice',
+        'user_id':2,
+         'comment': "Sweet and tangy orange juice, bonus that you can see into the open kitchen where they squeeze out the oranges!",
+         'ratings': 5,
+         'photo': None}]
+
     # Main Menu
     mainMenu_menuItems = [
         {'menuItemID': 'Spaghetti',
          'description': 'Drumwheat pasta with ragu sauce and parmesan cheese.',
          'price': 10.99,
-         'photo': None},
+         'photo': None,
+         'reviews': spaghetti_reviews},
         {'menuItemID': 'Chicken Burger',
          'description': 'Chicken burger pate with tomatoes, lettuce and ketchup in a brioche bun.',
          'price':7.49,
-         'photo': None},
+         'photo': None,
+         'reviews': chickenBurger_reviews},
         {'menuItemID': 'Sweet Potato Fries',
          'description': 'Deep-fried thin sweet potato slices with cajun seasoning.',
          'price':5,
-         'photo': None}]
+         'photo': None,
+         'reviews': spFries_reviews}]
 
 
     # Drinks Menu
@@ -28,11 +83,13 @@ def populate():
         {'menuItemID': 'Orange Juice',
          'description': 'Freshly squeezed orange juice.',
          'price': 2.49,
-         'photo': None},
+         'photo': None,
+         'reviews': oj_reviews},
         {'menuItemID': 'Hot Chocolate',
          'description': 'Made with oat milk, topped with marshmallows and cream.',
          'price': 2.99,
-         'photo': None}]
+         'photo': None,
+         'reviews': hotChocolate_reviews}]
 
 
     # note: add more menus on this area when required
@@ -43,20 +100,32 @@ def populate():
 
     for menuID, menu_data in menus.items():
         m = add_menu(menuID)
-        for mI in menu_data['menuItems']:
-            add_menuItem(m, menuItemID = mI['menuItemID'], description=mI['description'], price=mI['price'], photo=mI['photo'])
+        for menuItem in menu_data['menuItems']:
+            mI = add_menuItem(m, menuItemID = menuItem['menuItemID'], description=menuItem['description'], price=menuItem['price'], photo=menuItem['photo'])
+            for review in menuItem['reviews']:
+                add_review(mI, reviewID = review['reviewID'], user_id = review['user_id'], comment = review['comment'], ratings = review['ratings'], photo = review['photo'])
+
 
     for m in Menu.objects.all():
         for mI in MenuItem.objects.filter(menu=m):
             print(f'- {m}: {mI}')
 
-# functions used above to add the pages and categories 
+# functions used above to add menus, menu items and reviews
+
+def add_review(menuItem, user_id, reviewID, comment, ratings, photo):
+    review = Review.objects.get_or_create(menuItem=menuItem, user_id=user_id, reviewID=reviewID)[0]
+    review.comment = comment
+    review.ratings = ratings
+    review.photo = photo
+    review.save()
+    return review
+
 def add_menuItem(menu, menuItemID, description, price, photo):
     mI = MenuItem.objects.get_or_create(menu=menu, menuItemID=menuItemID)[0]
     mI.description=description
     mI.price = price
-    mI.photo = None;
-    mI.save
+    mI.photo = photo;
+    mI.save()
     return mI
 
 def add_menu(menuID):
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -9,6 +9,13 @@ urlpatterns = [
     #      views.show_category, name='show_category'),
     # path('add_category/', views.add_category, name='add_category'),
     # path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
+
+    # Add menu Item does not work because of missing menu_name_slug
+    path('add_menuItem/', views.add_menu_item, name="add_menuItem"),
+    # Works but it's empty at the moment
     path('menu/', views.show_menu, name='show_menu'),
+    #Template shows form but without any functionality
+    path('add_menu/', views.add_menu, name="add_menu"),
+    
     path('restricted/', views.restricted, name='restricted'),
 ]
